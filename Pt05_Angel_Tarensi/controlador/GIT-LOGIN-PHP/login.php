f72de18c87b614c7685195732ebda97224e5ce8e
atarensiSAPA <a.tarensi2@sapalomera.cat>
2023-11-12 13:46:42 +0100

    Insert de un readme.me, arreglar Oauth i recuperar contrasenya

---
+++ Pt05_Angel_Tarensi/controlador/GIT-LOGIN-PHP/login.php
@@ -0,0 +1,28 @@
+<?php
+/*!
+A simple example that shows how to use Guzzle as a Http Client for Hybridauth instead of PHP Curl extention.
+*/
+/*
+include 'vendor/autoload.php';
+
+$config = [
+    'callback' => "http://localhost/Practiques/Pt05_AlexVazquez/Controlador/github_controlador.php",
+
+    'keys' => ['id' => '6fd6e04c9fd6a8f40d50', 'secret' => '94ef8126e111ff22e7c774025daa88f0ed49d26a'],
+];
+
+try {
+    $adapter = new Hybridauth\Provider\Github($config);
+
+    $adapter->authenticate();
+
+    $tokens = $adapter->getAccessToken();
+    $userProfile = $adapter->getUserProfile();
+    $name = $userProfile->displayName;
+    $email = $userProfile->email;
+
+    $adapter->disconnect();
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
+*/
--- README.md
+++
@@ -1,3 +0,0 @@
-# IMPORTANT
-
-La carpeta Pt04_Angel_Tarensi és la part | i part || de l'activitat 4, per veure els còmits, fixat a la data d'aquells còmits i compara-ho amb la data d'entrega de l'activitat. Per obtenir la carpeta de la part | està pujada al Moodle a l'Activitat 4 com un .rar.
