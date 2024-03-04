7a22338e427d628e2c752839550e1ad9fdcee7a8
atarensiSAPA <a.tarensi2@sapalomera.cat>
2024-03-04 19:32:25 +0100

    canviar noms a la BD

--- Pt06_Angel_Tarensi/model/db-constants.php
+++ Pt06_Angel_Tarensi/model/db-constants.php
@@ -2,7 +2,7 @@
 //Angel Tarensi
 //Constants per la conexio a la BD
 define("DB_HOST" , "localhost");
-define("DB_NAME" , "Pt06_Angel_Tarensi");
+define("DB_NAME" , "pt06_angel_tarensi");
 define("DB_USER" , "root");
 define("DB_PASS" , "");
 ?>
